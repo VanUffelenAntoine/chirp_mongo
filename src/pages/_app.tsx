4718b78a296c77d0b663dd97f7575d915d9a5694
Antoine Van Uffelen <antoine@mansio.be>
2024-03-01 17:24:57 +0100

    THemed clerk added publicroute Added loadingspinners

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,12 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />;
+    <ClerkProvider {...pageProps} appearance={{ baseTheme: dark }}>
+      <Component {...pageProps} />
     </ClerkProvider>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ import { type RouterOutputs, api } from "~/utils/api";
 
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import { LoadingPage, LoadingSpinner } from "~/components/loading";
 
 dayjs.extend(relativeTime);
 
@@ -21,15 +22,20 @@ const CreatePostWizard = () => {
   if (!user) return null;
 
   return (
-    <div className="flex gap-8">
-      <Image
-        src={user.imageUrl}
-        alt="Profile picture"
-        className="h-14 w-14 rounded-full"
-        width={56}
-        height={56}
-      />
-      <input placeholder="Type Some emojis!" className="grow bg-transparent" />
+    <div className="border-b border-slate-400 p-4 ">
+      <div className="flex gap-8">
+        <Image
+          src={user.imageUrl}
+          alt="Profile picture"
+          className="h-14 w-14 rounded-full"
+          width={56}
+          height={56}
+        />
+        <input
+          placeholder="Type Some emojis!"
+          className="grow bg-transparent"
+        />
+      </div>
     </div>
   );
 };
@@ -49,22 +55,35 @@ const PostView = (props: PostWithUser) => {
       />
       <div className="flex flex-col">
         <div className="flex gap-1">
-          <span className="font-bold">{`@ ${author.username} `}</span>
+          <span className="font-bold">{`@${author.username} `}</span>
           <span className="font-thin">{` · ${dayjs(post.createdAt).fromNow()}`}</span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-xl">{post.content}</span>
       </div>
     </div>
   );
 };
 
-export default function Home() {
+const Feed = () => {
   const { data, isLoading } = api.post.getAll.useQuery();
 
-  if (isLoading) return <div>Loading...</div>;
-
+  if (isLoading) return <LoadingPage />;
   if (!data)
     return <div>Something went wrong, please try refreshing the page</div>;
+
+  return (
+    <div>
+      {[...data, ...data]?.map((fullPost) => (
+        <PostView {...fullPost} key={fullPost.post.id} />
+      ))}
+    </div>
+  );
+};
+
+export default function Home() {
+  const { isLoaded } = useUser();
+  //Fetch data early
+  api.post.getAll.useQuery();
 
   return (
     <>
@@ -76,25 +95,27 @@ export default function Home() {
       <main className="flex h-screen justify-center">
         <div className="h-full w-full border-x border-slate-400 md:max-w-2xl">
           <div className="border-b border-slate-400 p-4 ">
-            <div className="flex justify-center">
-              <SignedIn>
-                {/* Mount the UserButton component */}
-                <UserButton />
-              </SignedIn>
-              <SignedOut>
-                {/* Signed out users get sign in button */}
-                <SignInButton />
-              </SignedOut>
+            <div className="flex justify-end">
+              <div>
+                {!isLoaded ? (
+                  <LoadingSpinner size={8} />
+                ) : (
+                  <>
+                    <SignedIn>
+                      {/* Mount the UserButton component */}
+                      <UserButton />
+                    </SignedIn>
+                    <SignedOut>
+                      {/* Signed out users get sign in button */}
+                      <SignInButton />
+                    </SignedOut>
+                  </>
+                )}
+              </div>
             </div>
           </div>
-          <div className="border-b border-slate-400 p-4 ">
-            <CreatePostWizard />
-          </div>
-          <div>
-            {[...data, ...data]?.map((fullPost) => (
-              <PostView {...fullPost} key={fullPost.post.id} />
-            ))}
-          </div>
+          <CreatePostWizard />
+          <Feed />
         </div>
       </main>
     </>
